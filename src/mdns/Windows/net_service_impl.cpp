02ed9754f04dbd89a93b77eae7702e95eef00ae5
Sheen Tian Shen <sheentianshen@gmail.com>
2018-10-28 17:28:01 +0800

    Fix folder name

---
+++ src/mdns/Windows/net_service_impl.cpp
@@ -0,0 +1,27 @@
+#include "../net_service_impl.h"
+
+net_service::implementation::implementation(const std::string& type)
+    : type_(type)
+{
+
+}
+
+net_service::implementation::~implementation()
+{
+
+}
+
+void net_service::implementation::add_txt_record(const std::string& k, const std::string& v)
+{
+
+}
+
+bool net_service::implementation::publish(const std::string& name, const uint16_t port)
+{
+    return true;
+}
+
+void net_service::implementation::suppress()
+{
+
+}
