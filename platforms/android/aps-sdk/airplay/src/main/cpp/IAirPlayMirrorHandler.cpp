36451528a914a4f194a266648ae2a33d506d44d4
Sheen Tian Shen <sheentianshen@gmail.com>
2019-01-07 12:59:37 +0800

    Convert data timestamp data type

--- platforms/android/aps-sdk/airplay/src/main/cpp/IAirPlayMirrorHandler.cpp
+++ platforms/android/aps-sdk/airplay/src/main/cpp/IAirPlayMirrorHandler.cpp
@@ -71,8 +71,6 @@ void IAirPlayMirrorHandler::on_mirror_stream_data(
                               (jbyte *)(p->payload));
       jlong timestamp = (uint32_t)normalize_ntp_to_ms(p->timestamp);
       // convert the value
-      __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG,
-                          "================================== Mirror stream data packet timestamp: %ld", timestamp);
       env->CallVoidMethod(jvm_obj_, mid, byte_array, (jlong)timestamp);
       env->DeleteLocalRef(byte_array);
     } else {
@@ -186,8 +184,6 @@ void IAirPlayMirrorHandler::on_audio_stream_data(
       jbyteArray byte_array = env->NewByteArray(payload_length);
       env->SetByteArrayRegion(byte_array, 0, payload_length,
                               (jbyte *)(p->payload));
-      __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG,
-                          "================================== Audio stream data packet timestamp: %ld", p->timestamp);
       env->CallVoidMethod(jvm_obj_, mid, byte_array, (jlong)(p->timestamp));
       env->DeleteLocalRef(byte_array);
     } else {
