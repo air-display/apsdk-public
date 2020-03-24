3ef01cf11637dd52ab1300187c8b618a7961aaad
Sheen Tian Shen <sheentianshen@gmail.com>
2020-03-24 15:32:33 +0800

    adjust the camke file

--- generate_linux.sh
+++
@@ -1,5 +0,0 @@
-cmake \
-    -S . \
-    -B .build.linux \
-    -G "Unix Makefiles" \
-    -DBUILD_APS_DEMO=FALSE
---
+++ generate_linux_proj.sh
@@ -0,0 +1,1 @@
+cmake -S . -B .build.linux -G "Unix Makefiles" -DBUILD_APS_DEMO=FALSE
