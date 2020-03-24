81f6c71e821f1c44e4febe9402cb3bce1966b50c
Sheen Tian Shen <sheen.tian@outlook.com>
2020-03-24 12:25:00 +0800

    fix double release

--- src/mdns/macOS/net_service_impl.mm
+++ src/mdns/macOS/net_service_impl.mm
@@ -2,25 +2,32 @@
 
 #include "../net_service_impl.h"
 
-class net_service::net_service_impl : public net_service::implementation { 
+class net_service::net_service_impl : public net_service::implementation {
 public:
   net_service_impl(const std::string &type) {
     type_ = [NSString stringWithUTF8String:type.c_str()];
     txt_records_ = [NSMutableDictionary dictionaryWithCapacity:0];
   }
 
-  ~net_service_impl() {}
+  ~net_service_impl() {
+    if (txt_records_) {
+      [txt_records_ release];
+      txt_records_ = nullptr;
+    }
 
-  virtual void add_txt_record(const std::string &k,
-                              const std::string &v) override {
-    [txt_records_ setValue:[NSString stringWithUTF8String:v.c_str()]
-                    forKey:[NSString stringWithUTF8String:k.c_str()]];
+    if (type_) {
+      [type_ release];
+      type_ = nullptr;
+    }
+  }
+
+  virtual void add_txt_record(const std::string &k, const std::string &v) override {
+    [txt_records_ setValue:[NSString stringWithUTF8String:v.c_str()] forKey:[NSString stringWithUTF8String:k.c_str()]];
   }
 
   virtual bool publish(const std::string &name, const uint16_t port) override {
     NSString *n = [NSString stringWithUTF8String:name.c_str()];
-    net_service_ =
-        [[NSNetService alloc] initWithDomain:@"" type:type_ name:n port:port];
+    net_service_ = [[NSNetService alloc] initWithDomain:@"" type:type_ name:n port:port];
     NSData *txt_data = [NSNetService dataFromTXTRecordDictionary:txt_records_];
     [net_service_ setTXTRecordData:txt_data];
     [net_service_ publish];
@@ -31,6 +38,7 @@ public:
     if (net_service_) {
       [net_service_ stop];
       [net_service_ release];
+      net_service_ = nullptr;
     }
   }
 
@@ -40,7 +48,6 @@ private:
   NSMutableDictionary *txt_records_;
 };
 
-net_service::implementation *
-net_service::implementation::get(const std::string &type) {
+net_service::implementation *net_service::implementation::get(const std::string &type) {
   return new net_service_impl(type);
 }
