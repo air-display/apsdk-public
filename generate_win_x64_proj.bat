5ff0b47e2eb401d00f328023893fb9379d5c49e0
Sheen <sheentianshen@gmail.com>
2021-06-18 11:08:50 +0800

    Update target sdk version

--- generate_win_x64_proj.bat
+++ generate_win_x64_proj.bat
@@ -3,4 +3,5 @@ cmake ^
     -B .build.win.x64 ^
     -G "Visual Studio 16 2019" ^
     -A x64 ^
+    -DCMAKE_SYSTEM_VERSION=10.0 ^
     -DBUILD_APS_DEMO=TRUE
--- generate_win_x86_proj.bat
+++ generate_win_x86_proj.bat
@@ -3,4 +3,5 @@ cmake ^
     -B .build.win.x86 ^
     -G "Visual Studio 16 2019" ^
     -A Win32 ^
+    -DCMAKE_SYSTEM_VERSION=10.0 ^
     -DBUILD_APS_DEMO=TRUE
